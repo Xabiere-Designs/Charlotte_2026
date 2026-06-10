a1a941b7335a38f75de46a353a372295ca9b5106
Xabiere-Designs <xabieredesigns@gmail.com>
2026-06-10 11:00:00 -0400

    updated infra files

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,6 +1,26 @@
 module "vpc" {
   source = "git::https://github.com/Xabiere-Designs/Infrastructure_AWS.git//modules/vpc"
 
-  name     = "Charlotte_2026-vpc"
-  vpc_cidr = "10.0.0.0/16"
+  name = "charlotte-2026"
+  cidr = "10.0.0.0/16"
+  azs  = ["us-east-1a", "us-east-1b"]
+
+  tags = {
+    Project = "Charlotte_2026"
+    Owner   = "Corey"
+  }
+}
+
+module "three_tier_ec2" {
+  source = "git::https://github.com/Xabiere-Designs/Infrastructure_AWS.git//modules/three_tier_ec2"
+
+  project_name      = "charlotte-2026"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_id  = module.vpc.public_subnet_ids[0]
+  private_subnet_id = module.vpc.private_subnet_ids[0]
+
+  aws_ami       = var.aws_ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  my_ip_cidr    = var.my_ip_cidr
 }
